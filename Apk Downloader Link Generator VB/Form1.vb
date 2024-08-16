7abe3bbe62de70c3bd1b6cc48ca42e4c50a75f85
MONSTERMC <74623428+noradlb1@users.noreply.github.com>
2024-08-16 02:52:59 +0200

    Apk-Downloader-Link-Generator-VB
    Apk-Downloader-Link-Generator-VB

---
+++ Apk Downloader Link Generator VB/Form1.vb
@@ -0,0 +1,20 @@
+﻿'Telegram: @MONSTERMC
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' تأكد من أن TextBox1 يحتوي على رابط
+        If TextBox1.Text.Contains("details?id=") Then
+            ' استخراج الجزء بعد "details?id="
+            Dim extractedPart As String = TextBox1.Text.Split(New String() {"details?id="}, StringSplitOptions.None)(1)
+
+            ' إنشاء الرابط الجديد
+            Dim newUrl As String = "https://apps.evozi.com/apk-downloader/?id=" & extractedPart
+
+            ' وضع الرابط الجديد في TextBox2
+            TextBox2.Text = newUrl
+            Process.Start(TextBox2.Text)
+        Else
+            MessageBox.Show("الرابط غير صحيح. تأكد من احتوائه على 'details?id='")
+        End If
+        'اتمنى من الجميع التوفيق في كل شيئ
+    End Sub
+End Class
